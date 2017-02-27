1597c7d6252d3ddf96048b4e869d65b33df44437
Stephen Moore <stephen@delfick.com>
2017-02-27 21:29:22 +1100

    Another logging improvement

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
       name = "delfick_app"
-    , version = "0.9.1"
+    , version = "0.9.2"
     , py_modules = ['delfick_app']
 
     , install_requires =
       [ 'argparse'
       , 'delfick_error==1.7.6.1'
-      , 'delfick_logging==0.2'
+      , 'delfick_logging==0.2.1'
       ]
 
     , extras_require =
